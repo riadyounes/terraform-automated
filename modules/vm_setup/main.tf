d8d41df4b18b193843e80e56f08c497213d5f8b0
Riad Younes <50601899+riadyounes@users.noreply.github.com>
2022-07-05 19:15:22 -0300

    Update main.tf

--- modules/vm_setup/main.tf
+++ modules/vm_setup/main.tf
@@ -3,4 +3,22 @@ resource "aws_instance" "backend" {
   instance_type          = var.instance_type
   vpc_security_group_ids = var.instance_security_group
 
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y 
+    sudo apt install curl -y
+    sudo apt install git -y
+    sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+  EOF
 }
